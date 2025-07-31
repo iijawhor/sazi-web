e37759bc46912c68116818085461a9366ad07369
iijawhor <jawhorali1@gmail.com>
2025-08-01 00:45:10 +0530

    implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Body, Dashboard, Me, Signup } from "./allFiles";
+
 function App() {
   return (
-    <div className="h-full">
-      <h1 className="text-3xl text-red-400 font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            {/* Child Routes */}
+            <Route index element={<Dashboard />} /> {/* default child */}
+            <Route path="me" element={<Me />} />
+          </Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/me" element={<Me />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
