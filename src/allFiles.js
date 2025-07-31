d79370f044fcb285d6c5d97b71098abeaab10d34
iijawhor <jawhorali1@gmail.com>
2025-08-01 00:44:05 +0530

    added assets(img) in public file and added imports of debounce and other components like navbar me sidebar etc.

---
+++ src/allFiles.js
@@ -0,0 +1,9 @@
+import Body from "./pages/Body";
+import Sidebar from "./componenets/Sidebar/Sidebar";
+import Navbar from "./componenets/Navbar/Navbar";
+import Signup from "./componenets/Sidebar/Signup";
+import { LiveClock } from "./helpers/Liveclock";
+import Me from "./componenets/Students/Me";
+import Dashboard from "./componenets/Dashboard/Dashboard";
+import useDebounce from "./helpers/Debounce";
+export { Body, Sidebar, Navbar, Signup, LiveClock, Me, Dashboard, useDebounce };
