02302d5820751e61a9b3bdeb04d360dd2599ebe8
iijawhor <jawhorali1@gmail.com>
2025-08-04 23:01:56 +0530

    - Integrated Redux Toolkit for authentication:   - Created userSlice.js with async thunks for signup (signupUser) and login (signinUser)   - Implemented state for loading, error, token, and user info   - Persisted JWT token in localStorage for session handling - Configured Redux store in App.jsx:   - Wrapped entire app with <Provider store={store}>   - Integrated BrowserRouter and Routes for navigation - Updated allFiles.js for centralized imports/exports:   - Exported Signup, Dashboard, Me, Body, LiveClock, Actions for easy reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Body, Dashboard, Me, Signup } from "./allFiles";
-
+import { Provider } from "react-redux";
+import { store } from "./store/sotre.js";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            {/* Child Routes */}
-            <Route index element={<Dashboard />} /> {/* default child */}
-            <Route path="me" element={<Me />} />
-          </Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/me" element={<Me />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        {" "}
+        {/* ✅ Wrap with Provider */}
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              {/* Child Routes */}
+              <Route index element={<Dashboard />} /> {/* default child */}
+              <Route path="me" element={<Me />} />
+            </Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/me" element={<Me />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/allFiles.js
+++ src/allFiles.js
@@ -3,7 +3,18 @@ import Sidebar from "./componenets/Sidebar/Sidebar";
 import Navbar from "./componenets/Navbar/Navbar";
 import Signup from "./componenets/Signup/Signup";
 import { LiveClock } from "./helpers/Liveclock";
-import Me from "./componenets/Students/Me";
+import Me from "./componenets/Me/Me";
 import Dashboard from "./componenets/Dashboard/Dashboard";
 import useDebounce from "./helpers/Debounce";
-export { Body, Sidebar, Navbar, Signup, LiveClock, Me, Dashboard, useDebounce };
+import Actions from "./componenets/Me/Actions";
+export {
+  Body,
+  Sidebar,
+  Navbar,
+  Signup,
+  LiveClock,
+  Me,
+  Dashboard,
+  useDebounce,
+  Actions
+};
